12da56e097e562026b9172ce53ef9a48f2783b70
yui0722 <takahaimasato222@gmail.com>
2016-07-08 16:26:44 +0900

    更新

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 //app.js
-var gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+var gameArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17];
 var pickedTiles = [];
 var scoreText;
-var moves=0;
+var missText;
+var moves = 0;
+var miss = 0;
 var console_label;
 
-var shuffle = function(v){
-    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i),x = v[--i], v[i] = v[j], v[j] = x);
-        return v;
+var shuffle = function(v) {
+    for (var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+    return v;
 };
 
 var gameScene = cc.Scene.extend({
-    onEnter:function () {
+    onEnter: function() {
 
-      var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
+        var backgroundLayer = new cc.LayerColor(new cc.Color(140, 200, 140, 128));
         this.addChild(backgroundLayer);
 
         gameArray = shuffle(gameArray);
@@ -25,25 +27,25 @@ var gameScene = cc.Scene.extend({
 });
 
 var game = cc.Layer.extend({
-    init:function () {
+    init: function() {
         this._super();
-        var gradient = cc.LayerGradient.create(cc.color(0,0,0,255), cc.color(0x46,0x82,0xB4,255));
+        var gradient = cc.LayerGradient.create(cc.color(0, 0, 0, 255), cc.color(0x46, 0x82, 0xB4, 255));
         this.addChild(gradient);
-        scoreText = cc.LabelTTF.create("Moves: 0","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+        scoreText = cc.LabelTTF.create("MISS: 0", "Arial", "32", cc.TEXT_ALIGNMENT_CENTER);
         this.addChild(scoreText);
-        scoreText.setPosition(90,50);
-        for(i=0;i<16;i++){
+        scoreText.setPosition(90, 50);
+        for (i = 0; i < 36; i++) {
             var tile = new MemoryTile();
             tile.pictureValue = gameArray[i];
-            this.addChild(tile,0);
+            this.addChild(tile, 0);
             //タイルを格子状に配置する計算式
-            tile.setPosition(49+i%4*74,400-Math.floor(i/4)*74);
+            tile.setPosition(85 + i % 6 * 70, 550 - Math.floor(i / 6) * 85);
         }
     }
 });
 //Spriteクラスを拡張して実装してみた
 var MemoryTile = cc.Sprite.extend({
-    ctor:function() {
+    ctor: function() {
         this._super();
         this.initWithFile(res.cover_png);
         cc.eventManager.addListener(listener.clone(), this);
@@ -53,16 +55,16 @@ var MemoryTile = cc.Sprite.extend({
 var listener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
     swallowTouches: true,
-    onTouchBegan: function (touch, event) {
-        if(pickedTiles.length<2){
+    onTouchBegan: function(touch, event) {
+        if (pickedTiles.length < 2) {
             var target = event.getCurrentTarget();
             var location = target.convertToNodeSpace(touch.getLocation());
             var targetSize = target.getContentSize();
             var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
             if (cc.rectContainsPoint(targetRectangle, location)) {
-                target.initWithFile("res/tile_"+target.pictureValue+".png");
+                target.initWithFile("res/s_" + target.pictureValue + ".png");
                 pickedTiles.push(target);
-                if(pickedTiles.length==2){
+                if (pickedTiles.length == 2) {
                     checkTiles();
                 }
             }
@@ -70,18 +72,24 @@ var listener = cc.EventListener.create({
     }
 });
 
-function checkTiles(){
-    moves++;
-    scoreText.setString("Moves: "+moves);
-    var pause = setTimeout(function(){
-        if(pickedTiles[0].pictureValue!=pickedTiles[1].pictureValue){
+function checkTiles() {
+
+
+    var pause = setTimeout(function() {
+        if (pickedTiles[0].pictureValue != pickedTiles[1].pictureValue) {
             pickedTiles[0].initWithFile(res.cover_png);
             pickedTiles[1].initWithFile(res.cover_png);
+            miss++;
+            scoreText.setString("MISS: " + miss);
+            if (miss == 3) {
+                cc.director.runScene(new ThirdScene());
         }
-        else{
+
+        } else {
             gameLayer.removeChild(pickedTiles[0]);
             gameLayer.removeChild(pickedTiles[1]);
         }
+
         pickedTiles = [];
-    },2000);
+    }, 2000);
 }
--- src/resource.js
+++ src/resource.js
@@ -1,14 +1,24 @@
 //resource.js
 var res = {
     cover_png : "res/cover.png",
-    tile_0_png : "res/tile_0.png",
-    tile_1_png : "res/tile_1.png",
-    tile_2_png : "res/tile_2.png",
-    tile_3_png : "res/tile_3.png",
-    tile_4_png : "res/tile_4.png",
-    tile_5_png : "res/tile_5.png",
-    tile_6_png : "res/tile_6.png",
-    tile_7_png : "res/tile_7.png",
+    s_0_png : "res/s_0.png",
+    s_1_png : "res/s_1.png",
+    s_2_png : "res/s_2.png",
+    s_3_png : "res/s_3.png",
+    s_4_png : "res/s_4.png",
+    s_5_png : "res/s_5.png",
+    s_6_png : "res/s_6.png",
+    s_7_png : "res/s_7.png",
+    s_8_png : "res/s_8.png",
+    s_9_png : "res/s_9.png",
+    s_10_png : "res/s_10.png",
+    s_11_png : "res/s_11.png",
+    s_12_png : "res/s_12.png",
+    s_13_png : "res/s_13.png",
+    s_14_png : "res/s_14.png",
+    s_15_png : "res/s_15.png",
+    s_16_png : "res/s_16.png",
+    s_17_png : "res/s_17.png"
 };
 
 var g_resources = [];
